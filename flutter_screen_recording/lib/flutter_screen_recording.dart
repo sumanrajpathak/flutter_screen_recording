9e1a3d1728f6623b29e839e58c5930bde157310e
Suman Raj Pathak <pathaksumanraj@gmail.com>
2024-07-04 10:36:33 +0545

    title and message params added

--- flutter_screen_recording/lib/flutter_screen_recording.dart
+++ flutter_screen_recording/lib/flutter_screen_recording.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_screen_recording_platform_interface/flutter_screen_recording_platform_interface.dart';
-//import 'file:D:/Workspace/flutter_screen_recording/flutter_screen_recording_platform_interface/lib/flutter_screen_recording_platform_interface.dart';
 import 'dart:async';
 import 'dart:io';
 
 class FlutterScreenRecording {
-  static Future<bool> startRecordScreen(String name, {String? titleNotification, String? messageNotification}) async{
-    if(titleNotification == null ){
+  static Future<bool> startRecordScreen(String name,
+      {String? titleNotification, String? messageNotification}) async {
+    if (titleNotification == null) {
       titleNotification = "";
     }
-    if(messageNotification == null ){
+    if (messageNotification == null) {
       messageNotification = "";
     }
 
     await _maybeStartFGS(titleNotification, messageNotification);
-    final bool start = await FlutterScreenRecordingPlatform.instance.startRecordScreen(
+
+    final bool start =
+        await FlutterScreenRecordingPlatform.instance.startRecordScreen(
       name,
-      notificationTitle: titleNotification, 
+      notificationTitle: titleNotification,
       notificationMessage: messageNotification,
     );
 
     return start;
   }
 
-  static Future<bool> startRecordScreenAndAudio(String name, {String? titleNotification, String? messageNotification}) async {
+  static Future<bool> startRecordScreenAndAudio(String name,
+      {String? titleNotification, String? messageNotification}) async {
     //await _maybeStartFGS(titleNotification, messageNotification);
-    final bool start = await FlutterScreenRecordingPlatform.instance.startRecordScreenAndAudio(
+    final bool start =
+        await FlutterScreenRecordingPlatform.instance.startRecordScreenAndAudio(
       name,
       notificationTitle: titleNotification ?? "",
       notificationMessage: messageNotification ?? "",
@@ -35,16 +39,17 @@ class FlutterScreenRecording {
   }
 
   static Future<String> get stopRecordScreen async {
-    final String path = await FlutterScreenRecordingPlatform.instance.stopRecordScreen;
+    final String path =
+        await FlutterScreenRecordingPlatform.instance.stopRecordScreen;
     if (!kIsWeb && Platform.isAndroid) {
       FlutterForegroundTask.stopService();
     }
     return path;
   }
 
-  static  _maybeStartFGS(String titleNotification, String messageNotification) async {
+  static _maybeStartFGS(
+      String titleNotification, String messageNotification) async {
     if (!kIsWeb && Platform.isAndroid) {
-
       FlutterForegroundTask.init(
         androidNotificationOptions: AndroidNotificationOptions(
           channelId: 'notification_channel_id',
@@ -71,7 +76,7 @@ class FlutterScreenRecording {
           autoRunOnBoot: true,
           allowWifiLock: true,
         ),
-          //iosNotificationOptions:true,
+        //iosNotificationOptions:true,
         //intDevLog: true,
       );
     }
